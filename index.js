c5e16f41f99536131bfe8c6baca372db24f5c1fa
AndersUrban <anders.justurban@gmail.com>
2024-09-10 09:52:15 +0200

    Commit changes

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
 
+const express = require('express');
+const path = require('path');  // Importer 'path' til filhåndtering
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Server statiske filer fra mappen 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Standardrute til at vise dashboardet
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start serveren
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
